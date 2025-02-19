b4a670480b9131b79a4b500f726fa58b9491b05e
ttooaa2020 <sashin9622@gmail.com>
2025-02-19 17:51:54 +0900

    메인 비주얼 수정

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@ $(function () {
   const duration = 300;
 
   // 모바일 더보기
-  const btnMenu = document.querySelector(".mobile-btn");
-  const mobileMenu = document.querySelector(".mobile-menu");
+  const btnmenu = document.querySelector(".mobile-btn");
+  const mobilemenu = document.querySelector(".mobile-menu");
   const btnClose = document.querySelector(".mobile-btn-close");
 
-  btnMenu.addEventListener("click", () => {
-    mobileMenu.classList.add("active");
+  btnmenu.addEventListener("click", () => {
+    mobilemenu.classList.add("active");
   });
 
   btnClose.addEventListener("click", () => {
-    mobileMenu.classList.remove("active");
+    mobilemenu.classList.remove("active");
   });
   // 모바일 더보기 end
 
